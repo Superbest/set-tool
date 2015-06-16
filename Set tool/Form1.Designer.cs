6d6ebdfa1de741bb838a0d3709fccebed074a9e1
Superbest <arpharazon0@gmail.com>
2015-06-16 03:49:54 -0500

    Initial commit.
    Signed-off-by: Superbest <arpharazon0@gmail.com>

---
+++ Set tool/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Set_tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.tb_b_minus_a = new System.Windows.Forms.TextBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.tb_a_minus_b = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.tb_intersect = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.tb_union = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tb_set_b = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tb_set_a = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBox6, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox5, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox4, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(583, 558);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.tb_b_minus_a);
+            this.groupBox6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox6.Location = new System.Drawing.Point(294, 384);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(286, 171);
+            this.groupBox6.TabIndex = 7;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "B - A";
+            // 
+            // tb_b_minus_a
+            // 
+            this.tb_b_minus_a.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tb_b_minus_a.Location = new System.Drawing.Point(3, 16);
+            this.tb_b_minus_a.Multiline = true;
+            this.tb_b_minus_a.Name = "tb_b_minus_a";
+            this.tb_b_minus_a.Size = new System.Drawing.Size(280, 152);
+            this.tb_b_minus_a.TabIndex = 1;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.tb_a_minus_b);
+            this.groupBox5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox5.Location = new System.Drawing.Point(3, 384);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(285, 171);
+            this.groupBox5.TabIndex = 6;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "A - B";
+            // 
+            // tb_a_minus_b
+            // 
+            this.tb_a_minus_b.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tb_a_minus_b.Location = new System.Drawing.Point(3, 16);
+            this.tb_a_minus_b.Multiline = true;
+            this.tb_a_minus_b.Name = "tb_a_minus_b";
+            this.tb_a_minus_b.Size = new System.Drawing.Size(279, 152);
+            this.tb_a_minus_b.TabIndex = 1;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.tb_intersect);
+            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox4.Location = new System.Drawing.Point(294, 208);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(286, 170);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "A ∩ B";
+            // 
+            // tb_intersect
+            // 
+            this.tb_intersect.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tb_intersect.Location = new System.Drawing.Point(3, 16);
+            this.tb_intersect.Multiline = true;
+            this.tb_intersect.Name = "tb_intersect";
+            this.tb_intersect.Size = new System.Drawing.Size(280, 151);
+            this.tb_intersect.TabIndex = 1;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.tb_union);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(3, 208);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(285, 170);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "A U B";
+            // 
+            // tb_union
+            // 
+            this.tb_union.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tb_union.Location = new System.Drawing.Point(3, 16);
+            this.tb_union.Multiline = true;
+            this.tb_union.Name = "tb_union";
+            this.tb_union.Size = new System.Drawing.Size(279, 151);
+            this.tb_union.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tb_set_b);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(294, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(286, 170);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Set B";
+            // 
+            // tb_set_b
+            // 
+            this.tb_set_b.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tb_set_b.Location = new System.Drawing.Point(3, 16);
+            this.tb_set_b.Multiline = true;
+            this.tb_set_b.Name = "tb_set_b";
+            this.tb_set_b.Size = new System.Drawing.Size(280, 151);
+            this.tb_set_b.TabIndex = 1;
+            this.tb_set_b.TextChanged += new System.EventHandler(this.tb_set_b_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(285, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Clear);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(294, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(286, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Process";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Process);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tb_set_a);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(285, 170);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Set A";
+            // 
+            // tb_set_a
+            // 
+            this.tb_set_a.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tb_set_a.Location = new System.Drawing.Point(3, 16);
+            this.tb_set_a.Multiline = true;
+            this.tb_set_a.Name = "tb_set_a";
+            this.tb_set_a.Size = new System.Drawing.Size(279, 151);
+            this.tb_set_a.TabIndex = 0;
+            this.tb_set_a.TextChanged += new System.EventHandler(this.tb_set_a_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 558);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.TextBox tb_b_minus_a;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.TextBox tb_a_minus_b;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox tb_intersect;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox tb_union;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tb_set_b;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tb_set_a;
+
+
+    }
+}
+
---
+++ Set tool/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Set_tool
+{
+    public partial class Form1 : Form
+    {
+        private List<TextBox> text_boxes;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            text_boxes = tableLayoutPanel1.Controls.OfType<GroupBox>().Select(gb => gb.Controls.OfType<TextBox>().First()).ToList();
+
+            tb_set_a_TextChanged(null, null);
+            tb_set_b_TextChanged(null, null);
+            Update_result_box_titles(0, 0, 0, 0);
+        }
+
+        private List<string> Get_set_a()
+        {
+            var set_a = Parse_set(tb_set_a.Text);
+            return set_a;
+        }
+
+        private List<string> Get_set_b()
+        {
+            var set_b = Parse_set(tb_set_b.Text);
+            return set_b;
+        }
+
+        private List<string> Parse_set(string text)
+        {
+            var set = text.Replace("\r\n", "\n").Split('\n').Select(s => s.Trim()).Where(s => s.Length > 0).ToList();
+            return set;
+        }
+
+        private void Process(object sender, EventArgs e)
+        {
+            var a = Get_set_a();
+            var b = Get_set_b();
+
+            var union = a.Union(b).ToList();
+            tb_union.Text = string.Join(Environment.NewLine, union);
+
+            var intersection = a.Intersect(b).ToList();
+            tb_intersect.Text = string.Join(Environment.NewLine, intersection);
+
+            var a_minus_b = a.Except(b).ToList();
+            tb_a_minus_b.Text = string.Join(Environment.NewLine, a_minus_b);
+
+            var b_minus_a = b.Except(a).ToList();
+            tb_b_minus_a.Text = string.Join(Environment.NewLine, b_minus_a);
+
+            Update_result_box_titles(union.Count, intersection.Count, a_minus_b.Count, b_minus_a.Count);
+        }
+
+        private void Update_result_box_titles(int union, int intersection, int a_minus_b, int b_minus_a)
+        {
+            groupBox3.Text = "A U B: " + union + " items";
+            groupBox4.Text = "A ∩ B: " + intersection + " items";
+            groupBox5.Text = "A - B: " + a_minus_b + " items";
+            groupBox6.Text = "B - B: " + b_minus_a + " items";
+        }
+
+        private void Clear(object sender, EventArgs e)
+        {
+            foreach (var tb in text_boxes) tb.Clear();
+        }
+
+        private void tb_set_a_TextChanged(object sender, EventArgs e)
+        {
+            groupBox1.Text = "Set A: " + Get_set_a().Count() + " items";
+        }
+
+        private void tb_set_b_TextChanged(object sender, EventArgs e)
+        {
+            groupBox2.Text = "Set B: " + Get_set_b().Count() + " items";
+        }
+    }
+}
